451deca9f54f9b7212147822a3357d42a97b9ac1
Deepak Chand <deepu996gmail.com>
2018-07-21 00:44:26 +0530

    Created user login logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,6 +16,7 @@ router.get("/test", (req, res) => res.json({ msg: "User Works" }));
 // @access  Public
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
+    //Creating User
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
@@ -30,7 +31,7 @@ router.post("/register", (req, res) => {
         avatar,
         password: req.body.password
       });
-
+      //Password Encryption
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
@@ -42,6 +43,30 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    User login / Returning JWT(Json web token) Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    //Check for password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
   });
 });
 
